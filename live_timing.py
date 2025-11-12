2e60e8b897318de86475f81b509c10d85773e4fa
Rodney Smith <38564867+Rodney-Smith@users.noreply.github.com>
2025-11-12 10:28:58 -0800

    Update live_timing.py
    added import os

--- live_timing.py
+++ live_timing.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
+import os
 from datetime import datetime, timedelta, timezone
 import threading
 import time
@@ -197,5 +198,6 @@ def live_session_data_route(session_filter, driver_filter):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
+
 
 
